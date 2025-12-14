6694f8431272c2f9b31001a11d243445c3ce438d
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2025-12-14 17:03:30 +0530

    cors_update_BACKEND

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable CORS
+  app.enableCors({
+    origin: '*', // Replace with your frontend URL (Next.js)
+    methods: 'GET, POST, PUT, DELETE', // allow specific HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // allow specific headers
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
